e97497e4d627e3a429d1a55315ff6a0116266212
Gean <gean.96@live.com>
2023-05-23 10:59:05 -0300

    fix: Finalized portfolio

---
+++ src/utils/userData.ts
@@ -0,0 +1,7 @@
+export const userData = {
+  nameUser: "Gean Santos",
+  githubUser: "geansdev",
+  linkedinUser: "geanssantos",
+  whatsappNumber: "+5551997723614",
+  emailUser: "gean.96@live.com",
+};
